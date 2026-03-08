f7e1638a83fb890434cf1de18d5aefef11f2d1ea
Professor de Matemática <cleutonlima06@gmail.com>
2026-03-08 03:37:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9279349983-4c741",
+  "appId": "1:172924187019:web:7e234b2fc68f11d3b2bf8e",
+  "apiKey": "AIzaSyAQTVoPscOzx_J5vHwaVxvSo_akTeXr0iY",
+  "authDomain": "studio-9279349983-4c741.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "172924187019"
+};
